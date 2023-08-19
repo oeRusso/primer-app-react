d6f0cf60d86fd822bfcad6ba87ceb5c7f4387577
Esteban Russo <oerusso28@gmail.com>
2023-08-19 11:01:09 -0300

    primer app en react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import TaskList from './TaskList';
+import TaskForm from './TaskForm';
+
+function HolaMundo() {
+  return (
+    <>
+      <TaskForm/>
+      <TaskList/>
+    </>
+  )
+}
+
+export default HolaMundo
---
+++ src/task.js
@@ -0,0 +1,32 @@
+export const tasks = [
+  {
+    id: 0,
+    title: 'mi primer tarea',
+    description: 'mi primer tarea'
+  },
+  {
+    id: 1,
+    title: 'mi segundax tarea',
+    description: 'mi segundax tarea'
+  },
+  {
+    id: 2,
+    title: 'mi tercer tarea',
+    description: 'mi tercer tarea'
+  },
+  {
+    id: 3,
+    title: 'mi primer tarea',
+    description: 'mi primer tarea'
+  },
+  {
+    id: 4,
+    title: 'mi segundax tarea',
+    description: 'mi segundax tarea'
+  },
+  {
+    id: 5,
+    title: 'mi tercer tarea',
+    description: 'mi tercer tarea'
+  }
+]
